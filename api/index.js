90a7bf413635116f5f798d8af612925fb9753b0f
Federico Giovenco <giovencofede@gmail.com>
2021-07-07 21:42:23 -0300

    adding changes to finish deploy on heroku

--- api/index.js
+++ api/index.js
@@ -18,8 +18,9 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./app.js');
+require("dotenv").config();
 
 // Syncing all the models at once.
-server.listen(3001, () => {
-  console.log('%s listening at 3001'); // eslint-disable-line no-console
+server.listen(process.env.PORT, () => {
+  console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
 });
